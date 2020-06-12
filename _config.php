e07fd748d51e612840e46bdef724a3af5e17c58f
ss-koshala <koshala.manojeewa@silverstripers.com>
2020-06-12 09:55:48 +0530

    error fix with module name

--- _config.php
+++ _config.php
@@ -10,7 +10,7 @@ if (!file_exists(BASE_PATH . DIRECTORY_SEPARATOR . DMS_DIR)) {
 
 // Ensure compatibility with PHP 7.2 ("object" is a reserved word),
 // with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
-if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+//if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
 
 \SilverStripe\Admin\CMSMenu::remove_menu_item('DMSDocumentAddController');
 
