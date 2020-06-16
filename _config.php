5efe9f8195cd2eb492efa358ab53197336e1d6ec
ss-koshala <koshala.manojeewa@silverstripers.com>
2020-06-16 07:27:18 +0530

    bug fixing

--- _config.php
+++ _config.php
@@ -1,6 +1,13 @@
 <?php
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Admin\CMSMenu;
+use SilverStripe\View\Parsers\ShortcodeParser;
+use SilverStripeDMS\Model\DMSDocument;
+use SilverStripeDMS\Model\DMSDocument_versions;
+use SilverStripeDMS\DMS;
+use SilverStripe\Assets\Filesystem;
 
-$config = \SilverStripe\Core\Config\Config::inst();
+$config = Config::inst();
 
 define('DMS_DIR', basename(__DIR__));
 
@@ -12,20 +19,21 @@ if (!file_exists(BASE_PATH . DIRECTORY_SEPARATOR . DMS_DIR)) {
 // with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
 //if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
 
-\SilverStripe\Admin\CMSMenu::remove_menu_item('DMSDocumentAddController');
+CMSMenu::remove_menu_item('DMSDocumentAddController');
 
-\SilverStripe\View\Parsers\ShortcodeParser::get('default')->register(
-    $config->get('DMS', 'shortcode_handler_key'),
+ShortcodeParser::get('default')->register(
+    $config->get(DMS::class, 'shortcode_handler_key'),
     array('DMSShortcodeHandler', 'handle')
 );
 
-if ($config->get('DMSDocument_versions', 'enable_versions')) {
+if ($config->get(DMSDocument_versions::class, 'enable_versions')) {
     //using the same db relations for the versioned documents, as for the actual documents
-    $config->update('DMSDocument_versions', 'db', $config->get('DMSDocument', 'db'));
+    $config::modify(DMSDocument_versions::class, 'db',
+        $config->get(DMSDocument::class, 'db'));
 }
 
 // add dmsassets folder to file system sync exclusion
-if (strpos($config->get('DMS', 'folder_name'), 'assets/') === 0) {
-    $folderName = substr($config->get('DMS', 'folder_name'), 7);
-    $config->update('Filesystem', 'sync_blacklisted_patterns', array("/^" . $folderName . "$/i",));
+if (strpos($config->get(DMS::class, 'folder_name'), 'assets/') === 0) {
+    $folderName = substr($config->get(DMS::class, 'folder_name'), 7);
+    $config->modify(Filesystem::class, 'sync_blacklisted_patterns', array("/^" . $folderName . "$/i",));
 }
