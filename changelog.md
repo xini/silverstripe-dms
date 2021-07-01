69a2f0804c2685abdbb9c7bb588b9a336bcb0529
Florian Thoma <florian.thoma@innoweb.com.au>
2021-07-01 17:43:00 +1000

    fix readonly state of description field

--- changelog.md
+++ changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [3.2.1]
+
+* fix readonly state of description field
+
 ## [3.2.0]
 
 * add extension hook for DMS controller response
--- src/Extensions/FormFactoryExtension.php
+++ src/Extensions/FormFactoryExtension.php
@@ -8,11 +8,16 @@ use SilverStripe\Forms\TextareaField;
 
 class FormFactoryExtension extends Extension
 {
-    public function updateFormFields(FieldList $fields)
+    public function updateFormFields(FieldList $fields, $controller, $formName, $context)
     {
-        $fields->insertAfter(
-            'Title',
-            TextareaField::create('Description', _t(DMSDocument::class . '.DESCRIPTION', 'Description'))
-        );
+        $descField = TextareaField::create('Description', _t(DMSDocument::class . '.DESCRIPTION', 'Description'));
+
+        $titleField = $fields->fieldByName('Editor.Details.Title');
+        if ($titleField) {
+            if ($titleField->isReadonly()) {
+                $descField = $descField->performReadonlyTransformation();
+            }
+            $fields->insertAfter('Title', $descField);
+        }
     }
 }
